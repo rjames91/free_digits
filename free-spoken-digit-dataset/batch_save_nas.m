b40dd73025d68bed410ea8f2da265d357c5bff84
Juan P. Dominguez-Morales <juadommor2@gmail.com>
2017-10-27 15:50:13 +0200

    Changed output format
    Changed the format of the output. Now it generates a matrix for training, testing and their corresponding labels.

--- free-spoken-digit-dataset/batch_save_nas.m
+++ free-spoken-digit-dataset/batch_save_nas.m
@@ -2,18 +2,54 @@
 directory=['./NAS_recordings/more_reduc_vol50/left/'];
 files = dir(directory);
 
-for  i=1:size(files)  
+bin_width = 20;
+window_width = 50;
+num_channels = 64;
+
+A = [];
+B = [];
+
+for  i=1:size(files)
     if(~files(i).isdir)
-        fixed_NAS([directory,files(i).name],20,50,'left',64); 
+        processed_audio_file = fixed_NAS([directory,files(i).name],bin_width,window_width,'left',num_channels);
+        
+        A = [A, processed_audio_file];
     end
-end  
+end
+
+left_labels = zeros(2, size(A, 2)); 
+left_labels(1,:) = 1;
+
 
 directory=['./NAS_recordings/more_reduc_vol50/right/'];
 files = dir(directory);
 
-for  i=1:size(files)  
+for  i=1:size(files)
     if(~files(i).isdir)
-        fixed_NAS([directory,files(i).name],20,50,'right',64); 
+        processed_audio_file = fixed_NAS([directory,files(i).name],bin_width,window_width,'right',num_channels);
+        
+        B = [B, processed_audio_file];
     end
-end   
-  
+end
+
+right_labels = zeros(2, size(B, 2)); 
+right_labels(2,:) = 1;
+
+
+
+processed_samples = [A, B];
+labels = [left_labels, right_labels];
+
+
+learning_ratio = 85/100;
+
+k = randperm(size(processed_samples,2));
+train_x = processed_samples(:, k(1, 1:ceil(size(processed_samples, 2) * learning_ratio)));
+train_y = labels(:, k(1, 1:ceil(size(processed_samples, 2) * learning_ratio)));
+test_x = processed_samples(:, k(1, ceil(size(processed_samples, 2) * learning_ratio)+1: size(processed_samples,2)));
+test_y = labels(:, k(1, ceil(size(processed_samples, 2) * learning_ratio)+1: size(processed_samples,2)));
+
+%N=ceil(size(processed_samples, 2) * learning_ratio); % no. of rows needed
+%c=randperm(length(processed_samples),N);
+%train_x = processed_samples(:,c);  % output matrix
+%train_y = labels(:, c);
